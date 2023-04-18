d3d76cd3755eb4e88e5620aa9905e34f1cf58b6e
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-18 22:25:30 +0300

    add links to menu, compose page, and connect it to serber, add form inputs

--- app.js
+++ app.js
@@ -17,8 +17,22 @@ app.use(express.static("public"));
 app.get('/', function(req, res){
   res.render('home', {homeStartingContent:homeStartingContent});
 });
+
 app.get('/about', function(req, res){
   res.render('about', {aboutContent:aboutContent});
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact', {contactContent:contactContent});
+});
+
+app.get('/compose', function(req, res){
+  res.render('compose');
+});
+
+app.post('/compose', function(req, res){
+  console.log(req.body.titleInput);
+  console.log(req.body.postInput);
 });
 
 app.listen(3000, function() {
